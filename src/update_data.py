34e5698886eaa3bb6c4324ad4891336dfeb20650
ckdrkt2 <bbiuyy@gmail.com>
2022-09-17 15:56:30 +0000

    test

--- src/update_data.py
+++ src/update_data.py
@@ -5,10 +5,10 @@ import datetime
 import time
 
 # database
-conn = pymysql.connect(host='192.168.153.110', port=31802, user='root', password='tmaxfintech', db='finance', charset='utf8', autocommit=True, cursorclass=pymysql.cursors.DictCursor)
+conn = pymysql.connect(host='192.168.153.110', port=31802, user='root', password='tmaxfintech', db='financeDB', charset='utf8', autocommit=True, cursorclass=pymysql.cursors.DictCursor)
 cur = conn.cursor()
 
-today = datetime.date.today()
+today = datetime.date.today() - datetime.timedelta(days=1)
 stamp = int(time.mktime(datetime.date.today().timetuple()))
 
 info = {'ADA': 7, 'BOND10Y': 6, 'BOND1M': 6, 'BOND1Y': 6, 'BOND30Y': 6, 'BOND5Y': 6, 'BOND6M': 6, 'BTC': 7, 'DOW': 7, 'ETH': 7, 'EU': 7, 'FRANCE': 7, 'GAS': 7, 'GERMANY': 6, 'GOLD': 7, 'HG': 7, 'KOSPI': 7, 'NIKKEI': 6, 'OIL': 7, 'SP500': 6, 'SSEC': 7, 'UK': 6, 'USD/EUR': 6, 'USD/KRW': 6, 'USD/RUB': 6, 'VIX': 6, 'XRP': 7}
