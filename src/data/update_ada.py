b4a38ddfafbec856cab0c928f9429db04cb64bf1
ckdrkt2 <bbiuyy@gmail.com>
2022-09-20 05:41:00 +0000

    1min pipeline test

---
+++ src/data/update_ada.py
@@ -0,0 +1,32 @@
+import pymysql
+import requests
+import pandas as pd
+from sqlalchemy import create_engine
+from time import sleep
+
+# get last data timestamp
+conn = pymysql.connect(host='192.168.153.110', port=31802, user='root', password='tmaxfintech', db='COINS', charset='utf8', autocommit=True, cursorclass=pymysql.cursors.DictCursor)
+cur = conn.cursor()
+cur.execute("select max(Time) from ADA;")
+start = cur.fetchall()[0]['max(Time)'] + 60000
+
+# update ada data
+symbol = 'ADAUSDT'
+interval = '1m'
+
+while True:
+
+    url = "https://api.binance.com/api/v3/uiKlines?symbol={}&interval={}&startTime={}&limit=1000".format(symbol, interval, start)
+    headers = {"accept": "application/json"}
+    response = requests.get(url, headers=headers)
+
+    df = pd.DataFrame(response.json())
+    df.columns = ['Time', 'Open', 'High', 'Low', 'Close', 'Volume', 'Close Time', 'Quote', 'Trades', 'tb_base_av', 'tb_quote_av', 'Unused']
+    df = df.drop(['Unused', 'Close Time'], axis=1)
+
+    if len(df) > 0:
+        db_connection = create_engine('mysql+pymysql://root:tmaxfintech@192.168.153.110:31802/COINS')
+        df.to_sql(name='ADA', con=db_connection, if_exists='append', index=False)
+
+    sleep(20)
+    break
--- src/update_data.py
+++ src/update_data.py
@@ -1,33 +1,31 @@
 import pymysql
+import requests
 import pandas as pd
-import yfinance as yf
-import datetime
-import time
+from sqlalchemy import create_engine
 
 # database
-conn = pymysql.connect(host='192.168.153.110', port=31802, user='root', password='tmaxfintech', db='financeDB', charset='utf8', autocommit=True, cursorclass=pymysql.cursors.DictCursor)
+conn = pymysql.connect(host='192.168.153.110', port=31802, user='root', password='tmaxfintech', db='COINS', charset='utf8', autocommit=True, cursorclass=pymysql.cursors.DictCursor)
 cur = conn.cursor()
 
-today = datetime.date.today() - datetime.timedelta(days=1)
-stamp = int(time.mktime(today.timetuple()))
+cur.execute("select max(Time) from ADA;")
 
-info = {'ADA': 7, 'BOND10Y': 6, 'BOND1M': 6, 'BOND1Y': 6, 'BOND30Y': 6, 'BOND5Y': 6, 'BOND6M': 6, 'BTC': 7, 'DOW': 7, 'ETH': 7, 'EU': 7, 'FRANCE': 7, 'GAS': 7, 'GERMANY': 6, 'GOLD': 7, 'HG': 7, 'KOSPI': 7, 'NIKKEI': 6, 'OIL': 7, 'SP500': 6, 'SSEC': 7, 'UK': 6, 'USD/EUR': 6, 'USD/KRW': 6, 'USD/RUB': 6, 'VIX': 6, 'XRP': 7}
-symbols = {'ADA': 'ADA-USD', 'BTC': 'BTC-USD', 'ETH': 'ETH-USD', 'XRP': 'XRP-USD', 'BOND5Y': '^FVX', 'BOND10Y': '^TNX', 'BOND30Y': '^TYX', 'DOW': '^DJI', 'GAS': 'NG=F', 'GOLD': 'GC=F', 'KOSPI': '^KS11', 'NIKKEI': '^N225', 'OIL': 'CL=F', 'SP500': '^GSPC', 'USD/EUR': 'EUR=X', 'USD/RUB': 'RUB=X', 'USD/KRW': 'KRW=X', 'VIX': '^VIX', 'UK': '^FTSE', 'GERMANY': '^GDAXI', 'HG': 'QC=F'}
+last = cur.fetchall()[0]['max(Time)'] + 60000
+symbol = 'ADAUSDT'
+interval = '1m'
 
-for col in info:
-    if col not in symbols: continue
+url = "https://api.binance.com/api/v3/uiKlines?symbol={}&interval={}&startTime={}&limit=1000".format(symbol, interval, last)
+headers = {"accept": "application/json"}
+response = requests.get(url, headers=headers)
 
-    symbol = symbols[col]
-    df = yf.download(symbol, start=today, end=today)
-    df = df.reset_index()
-    data = dict(df.iloc[0])
+df = pd.DataFrame(response.json())
+df.columns = ['Time', 'Open', 'High', 'Low', 'Close', 'Volume', 'Close Time', 'Quote', 'Trades', 'tb_base_av', 'tb_quote_av', 'Unused']
+df = df.drop(['Unused', 'Close Time'], axis=1)
 
-    if info[col] == 7:
-        query = "insert into `{}`(timestamp, Date, Close, Open, High, Low, volume) values (%s, %s, %s, %s, %s, %s, %s)".format(col)
-        cur.execute(query, [stamp, today, data['Close'], data['Open'], data['High'], data['Low'], data['Volume']])
+db_connection = create_engine('mysql+pymysql://root:tmaxfintech@192.168.153.110:31802/COINS')
+df.to_sql(name='ADA', con=db_connection, if_exists='append', index=False)
 
-    else:
-        query = "insert into `{}`(timestamp, Date, Close, Open, High, Low) values (%s, %s, %s, %s, %s, %s)".format(col)
-        cur.execute(query, [stamp, today, data['Close'], data['Open'], data['High'], data['Low']])
+# symbol = 'ADAUSDT'
+# interval = '1m'
+#
+# stamp = int(time.mktime(target.timetuple())*1000)
 
-    cur.fetchall()
